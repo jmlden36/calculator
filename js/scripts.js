c2b3cd7d1bf212f66cb2a8590ac73cc9d2d02350
Matt Luker <jamesmattluker@gmail.com>
2021-10-22 08:04:11 -0600

    Change interface to have radio choice for operations

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,11 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
-
 function remainder(number1, number2) {
   return number1 % number2;
 }
 
-function converttempCtoF(number1, number2) {
+function converttempCtoF(number1,) {
   return (number1 * (9 / 5)) + 32;
 }
 
@@ -28,41 +27,24 @@ function converttempCtoF(number1, number2) {
 
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventDefault();
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    let result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    } else {
+      alert("Error")
+    }
     $("#output").text(result);
   });
 });
 
-$(document).ready(function() {
-  $("form#subtract").submit(function(event) {
-   event.preventDefault();
-    const number1 = parseInt($("#subtract1").val());
-    const number2 = parseInt($("#subtract2").val());
-    const result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#multiply").submit(function(event) {
-   event.preventDefault();
-    const number1 = parseInt($("#multiply1").val());
-    const number2 = parseInt($("#multiply2").val());
-    const result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-});
-
-$(document).ready(function() {
-  $("form#divide").submit(function(event) {
-   event.preventDefault();
-    const number1 = parseInt($("#divide1").val());
-    const number2 = parseInt($("#divide2").val());
-    const result = divide(number1, number2);
-    $("#output").text(result);
-  });
-});
